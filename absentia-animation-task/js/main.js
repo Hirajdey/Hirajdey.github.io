f04536f233332d1ee1b504ec63ff0aea9b0a1f99
HirajDey <deyhiraj@gmail.com>
2018-09-21 14:15:15 +0530

    text animation

---
+++ absentia-animation-task/js/main.js
@@ -0,0 +1,15 @@
+
+
+(function(){
+
+	$('#animation1').click(function(){
+		$('.text-animation-one').toggleClass('addAnimate');
+	});
+
+
+	$('#animation2').click(function(){
+		$('.text-animation-slide').toggleClass('textSlideTop');
+	});
+
+
+})();
