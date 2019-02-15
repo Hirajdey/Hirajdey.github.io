fb6b69a4f57630d9a6f7f5148cbe2e8ce1def15e
HirajDey <deyhiraj@gmail.com>
2019-02-15 10:18:00 +0530

    Add files via upload

---
+++ kishore/hiraj-sweta/footer.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package kishore
+ */
+
+?>
+
+	</div><!-- #content -->
+<footer id="colophon" class="site-footer">
+		<div class="container">
+			<div class="row -sf">
+				<div class="f-logo col">
+					<img src="https://www.rainbirdhomestay.in/wp-content/themes/rainbird/img/icons/rainbird homestay logo.svg" alt="">
+
+					<ul class="k-social">
+					  <li class="facebook"><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+					  <li class="twitter"><a href="#" target="_blank"><i class="fab fa-pinterest-p"></i></a></li>
+					  <li class="linkedin"><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+					  <li class="googleplus"><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+					</ul>
+				</div><!-- /.f-logo -->
+				<div class="col f-links">
+					<div class="half">
+						<ul>
+							<li><a href="#">Diamonds</a></li>
+							<li><a href="#">Wedding & Engagement</a></li>
+							<li><a href="#">Collection</a></li>
+							<li><a href="#">About Us</a></li>
+						</ul>
+					</div><!-- /.half -->
+					
+				</div><!-- /.col -->
+				<div class="col f-links">
+					<div class="half">
+						<ul>
+							<li><a href="#">Diamonds</a></li>
+							<li><a href="#">Wedding & Engagement</a></li>
+							<li><a href="#">Collection</a></li>
+							<li><a href="#">About Us</a></li>
+						</ul>
+					</div><!-- /.half -->
+					
+				</div><!-- /.col -->
+				<div class="col f-contact">
+					<address>
+						Address: 102B City Road,<br>Beenleigh, QLD 4207<br>
+					</address>
+					<ul>
+					  <li class="facebook">Phone : <a href="tel:07 3287 1230" target="_blank">(07) 3287 1230</i></a></li>
+					  <li class="twitter">Email : <a href="mailto:info@hgj.com.au" target="_blank">info@hgj.com.au</i></a></li>
+					</ul>
+				</div><!-- /.col -->
+			</div><!-- /.row -->
+		</div><!-- /.container -->
+		<div class="site-info">
+			<p>Copyrights &copy; 2019, All Rights Reserved. Hatton Garden.  Powered by <a href="#">Dataquest</a></p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+
+<script
+	src="https://code.jquery.com/jquery-3.3.1.min.js"
+	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	crossorigin="anonymous">
+</script>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ kishore/hiraj-sweta/js/main.js
@@ -0,0 +1,139 @@
+$(document).ready(function() {
+     $(window).scroll(function(){
+  var sticky = $('.header'),
+      scroll = $(window).scrollTop();
+
+   if (scroll >= 100) sticky.addClass('fixed');
+  else sticky.removeClass('fixed');
+});
+(function(){
+
+    const heroBannerSlider = document.querySelectorAll('.catlink a');
+    heroBannerSlider[0].classList.add('actives');
+
+  })();
+
+  $('.produts-heading li a').click(function() {
+
+    $('.produts-heading li a.active').removeClass('active');
+    $(this).addClass('active');
+});
+  
+    /*================================
+  =    hamburger-menu           =
+  ================================*/
+     $('.hamburger-menu').click(function(event) {
+        $(this).toggleClass('mm-close');
+        $('.navigations ul').slideToggle();
+      });
+    
+       /*================================
+  =    owl-carousel-testimonials      =
+  ================================*/
+
+  $('.pb-testimonial').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay : true,
+    dots: false,
+    nav: true,
+    navText:["<div class='fas fa-angle-left'></div>","<div class='fas fa-angle-right'></div>"],
+    navClass: ['owl-prev', 'owl-next'],
+    responsive:{
+        0:{
+            items:1
+        },
+    }
+})
+      /*================================
+  =    Banner Carousel         =
+  ================================*/ 
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+   
+    dots:true,
+    center: true,
+	margin: 0,
+	lazyLoad:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+  /*================================
+  =    Products Filtering          =
+  ================================*/
+  (function(){
+
+    let filter = '';
+    // const typeLinks = document.querySelectorAll('.typeLink a');
+    const catlinks = document.querySelectorAll('.catlink a');
+    const productCols = document.querySelectorAll('.blogarticlecol');
+    
+    // default listing
+    // for(let prdCol of productCols){
+    //   const pId = prdCol.getAttribute('id');
+    //   if (pId.toUpperCase().includes('Commercial'.toUpperCase())) {
+    //     prdCol.style.display = 'flex';
+    //   } 
+    //   else {
+    //     prdCol.style.display = 'none';
+    //   }
+    // }
+
+    // for top menu filtering
+    for(let catlink of catlinks){
+      catlink.onclick = function(){
+        const thisText = this.textContent;
+        for(let prdCol of productCols){
+          const thisAttr = prdCol.getAttribute('id');
+
+          if (filter) {
+            if(thisAttr.toUpperCase().includes(thisText.toUpperCase()) && thisAttr.toUpperCase().includes(filter.toUpperCase())){
+                prdCol.style.display = 'block'; 
+              }
+              else{
+               prdCol.style.display = 'none';
+              }
+          } else {
+              if(thisAttr.toUpperCase().includes(thisText.toUpperCase())){
+                prdCol.style.display = 'block'; 
+              }
+              else{
+               prdCol.style.display = 'none';
+              }
+          }
+        }     
+      }
+    }
+    // for box filtering
+    for(let typeLink of typeLinks){
+      typeLink.onclick = function(){
+        const thisText = this.textContent;
+        filter = 'thisText';
+        for(let prdCol of productCols){
+          const thisAttr = prdCol.getAttribute('id');
+          if(thisAttr.toUpperCase().includes(thisText.toUpperCase())){
+            prdCol.style.display = 'block'; 
+          }
+          else{
+             prdCol.style.display = 'none';
+          }
+        }     
+      }
+    }
+    
+  })();
+
+
+ 
+  
+   });
